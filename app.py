27325a525cdf89f5f793305bd77ca195df1cb58f
Marcilio <marciliopi@hotmail.com>
2024-11-03 17:50:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,28 @@ from tkinter import messagebox
 import openpyxl, xlrd
 import pathlib
 from openpyxl import workbook
+
+#Setando a aprencia padrão do sistema
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.layout_config()
+        self.appearence()
+
+    def layout_config(self):
+        self.title("Sistema de Gestão de Clientes:")
+        self.geometry("700x500")
+    
+    def appearence(self):
+        self.lb_apm = ctk.CTkLabel(self, text="tema", bg_color="transparent", text_color=['#000', "#fff"]).place(x=50, y=430)
+        self.opt_apm = ctk.CTkOptionMenu(self, values=["Light", "Dark", "System"], command=self.change_apm).place(x=50, y=460)
+
+    def change_apm(self, nova_aparencia):
+        ctk.set_appearance_mode(nova_aparencia)
+
+if __name__=="__main__":
+    app = App()
+    app.mainloop()
